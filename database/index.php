3959dea6563db30bd7845a135f7093bd7e33db3a
Belingheri <33315640+Belingheri@users.noreply.github.com>
2020-05-20 20:02:45 +0200

    Update index.php

--- database/index.php
+++ database/index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "ztlbergamo";
-$password = "riccardo123";
+$username = "*******";
+$password = "******";
 $dbname = "ztlbergamo";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
